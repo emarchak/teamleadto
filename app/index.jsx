1ca67b0c3be500de385004d22bbd9f3e47c66e56
emarchak <erin.marchak@shopify.com>
2019-04-04 16:53:04 -0400

    Basic form behaviour

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,80 @@
-import "./style.scss"
+import './style.scss';
 
-export default () => (
-  <h1>Heading</h1>
-)
+import { Helmet } from 'react-helmet';
+import { Component } from 'react';
+const text = {
+  title: 'TeamLeadTO',
+  emailLabel: 'Email',
+  emailPlaceholder: 'leaders@teamlead.to',
+  submitLabel: 'Join our slack community',
+};
+
+const Metadata = () => {
+  return (
+    <Helmet>
+      <meta charSet="UTF-8"/>
+      <title>{text.title}</title>
+    </Helmet>
+  )
+};
+
+class SlackForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({[evt.target.name]: evt.target.value});
+  }
+
+  handleSubmit(evt) {
+    console.log(this.state);
+    evt.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className={'slackform'} onSubmit={this.handleSubmit}>
+        <label>
+          {text.emailLabel}
+          <input type={'email'}
+                 className={'slackform--email'}
+                 name={'email'}
+                 placeholder={text.emailPlaceholder}
+                 value={this.state.email}
+                 autoFocus={true}
+                 onChange={this.handleChange}
+          />
+        </label>
+        <input type={'submit'} value={text.submitLabel} className={'slackform--submit'}/>
+      </form>
+    )
+  };
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <Metadata/>
+        <div className={'container'}>
+          <img src={'img/logo--dark.svg'} alt={text.title} className={'logo'} width={480}/>
+          <SlackForm email={this.state.email}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
